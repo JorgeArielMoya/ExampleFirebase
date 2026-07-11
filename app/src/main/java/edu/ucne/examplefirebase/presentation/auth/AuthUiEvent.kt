1b4c631a40777333d41f2fffa527fc53fa724ee1
jorge <jorge@LAPTOP-D8GDLQRL>
2026-07-11 12:42:34 -0400

    Agrega los posibles eventos y estados del log in

--- app/src/main/java/edu/ucne/examplefirebase/presentation/auth/AuthUiEvent.kt
+++ app/src/main/java/edu/ucne/examplefirebase/presentation/auth/AuthUiEvent.kt
@@ -1,4 +1,8 @@
 package edu.ucne.examplefirebase.presentation.auth
 
-class AuthUiEvent {
+import android.content.Context
+
+sealed class AuthUiEvent {
+    data class SignInWithGoogle(val context: Context) : AuthUiEvent()
+    object SignOut : AuthUiEvent()
 }
--- app/src/main/java/edu/ucne/examplefirebase/presentation/auth/AuthUiState.kt
+++ app/src/main/java/edu/ucne/examplefirebase/presentation/auth/AuthUiState.kt
@@ -1,4 +1,9 @@
 package edu.ucne.examplefirebase.presentation.auth
 
-class AuthUiState {
-}
+import com.google.firebase.auth.FirebaseUser
+
+data class AuthUiState(
+    val isLoading: Boolean = false,
+    val user: FirebaseUser? = null,
+    val errorMessage: String? = null
+)
