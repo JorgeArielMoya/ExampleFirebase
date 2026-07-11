184713dfcb64ab61f4376a7ba4552f8fa39795a5
jorge <jorge@LAPTOP-D8GDLQRL>
2026-07-11 12:49:26 -0400

    Llama la pantalla de autenticacion y coloca la notacion de AndroidEntryPoint

--- app/src/main/java/edu/ucne/examplefirebase/MainActivity.kt
+++ app/src/main/java/edu/ucne/examplefirebase/MainActivity.kt
@@ -11,37 +11,19 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
+import edu.ucne.examplefirebase.presentation.auth.AuthScreen
 import edu.ucne.examplefirebase.ui.theme.ExampleFirebaseTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             ExampleFirebaseTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                AuthScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ExampleFirebaseTheme {
-        Greeting("Android")
     }
 }
